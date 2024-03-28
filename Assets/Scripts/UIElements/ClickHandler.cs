7070924d1a0b5a8b9f7e34b56a991461a74125a1
W1RDy <ivan.alchin5@mail.ru>
2024-03-28 14:16:54 +0500

    Add click feature

---
+++ Assets/Scripts/UIElements/ClickHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+public class ClickHandler : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] Canvas _canvas;
+    [SerializeField] GameObject _clickView;
+    private CustomCamera _customCamera;
+
+    [Inject]
+    private void Construct(CustomCamera camera)
+    {
+        _customCamera = camera;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+        HandleClick();
+    }
+
+    public void HandleClick()
+    {
+        Ray ray = _customCamera.MainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 100, 1 << 5);
+
+        if (hit.collider != null)
+        {
+            var cube = Instantiate(_clickView, hit.point, Quaternion.identity, _canvas.transform);
+            if (hit.collider.CompareTag("CancelClick"))
+            {
+                Debug.Log("Click cancelled");
+                return;
+            }
+            Debug.Log("Click on " + hit.collider.gameObject.name);
+        }
+    }
+}
--- Assets/Scripts/UIElements/WindowService.cs
+++ Assets/Scripts/UIElements/WindowService.cs
@@ -29,5 +29,6 @@ public enum WindowType
     PauseWindow,
     MenuWindow,
     SettingsWindow,
-    ResetDataWindow
+    ResetDataWindow,
+    MapWindow
 }
