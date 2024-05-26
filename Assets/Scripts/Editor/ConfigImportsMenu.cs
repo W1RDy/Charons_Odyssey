5ef5332de9d9c6bfb2ab261d02d4abcc57ce3128
W1RDy <ivan.alchin5@mail.ru>
2024-05-26 18:34:29 +0500

    Update dialogs import feature.

---
+++ Assets/Scripts/Editor/ConfigImportsMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ConfigImportsMenu
+{
+    private const string CredentialsPath = "charons-odyssey-f6695769e714.json";
+    private const string SpreadsheetId = "1YMhx3uEUB3Czt7ZFJdGi0Q33sO2JEo2rDnDwyptJcT8";
+    private const string ItemsSheetsName = "Лист1";
+
+    private const string DialogFilePath = "Configs/DialogFile.json";
+
+    [MenuItem("Import/Imort Dialogs")]
+    private static async void LoadDialogsFromGoogle()
+    {
+        var dialogConfigs = LoadDialogsFromJSON();
+        var dialogParser = new DialogParser(dialogConfigs);
+
+        var sheetsImporter = new GoogleSheetsImporter(CredentialsPath, SpreadsheetId);
+        await sheetsImporter.DownloadAndParseSheet(ItemsSheetsName, dialogParser);
+
+        SaveDialogsToJSON(dialogConfigs);
+    }
+
+    private static DialogConfigs LoadDialogsFromJSON()
+    {
+        var json = File.ReadAllText("Assets/Resources/" + DialogFilePath);
+        var dialogConfigs = !string.IsNullOrEmpty(json) ? JsonUtility.FromJson<DialogConfigs>(json) : new DialogConfigs();
+        return dialogConfigs;
+    }
+
+    private static void SaveDialogsToJSON(DialogConfigs dialogConfigs)
+    {
+        string resourcesPath = "Assets/Resources/" + DialogFilePath;
+        var jsonString = JsonUtility.ToJson(dialogConfigs);
+        File.WriteAllText(resourcesPath, jsonString);
+    }
+}
