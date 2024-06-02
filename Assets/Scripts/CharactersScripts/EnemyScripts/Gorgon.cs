e972a9aa98e5ecb2218b1ae35b393ee3fb1cde01
W1RDy <ivan.alchin5@mail.ru>
2024-06-02 22:48:35 +0500

    Update minotaur animations. Add gorgon view.

--- Assets/Scripts/CharactersScripts/EnemyScripts/Gorgon.cs
+++ Assets/Scripts/CharactersScripts/EnemyScripts/Gorgon.cs
@@ -1,4 +1,8 @@
-﻿public class Gorgon : UnmovableEnemy
-{
+﻿using UnityEngine;
 
+public class Gorgon : UnmovableEnemy
+{
+    [SerializeField] private Transform _shootPoint;
+
+    public Transform ShootPoint => _shootPoint;
 }
